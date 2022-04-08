9b5062401de47537aeef4098f15d33632f3ffd2f
kprathyusha <pratyu.kulkarni@gmail.com>
2022-04-08 16:16:43 -0400

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header"></header>
+            <header className="App-header">Hello</header>
         </div>
     );
 }
