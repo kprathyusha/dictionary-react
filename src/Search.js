f78a50c42d8a45978d9e85bff5c40d37d2dc2b1e
kprathyusha <pratyu.kulkarni@gmail.com>
2022-04-08 17:42:26 -0400

    API integration

--- src/Search.js
+++ src/Search.js
@@ -1,12 +1,20 @@
+import axios from "axios";
 import React, { useState } from "react";
 import "./Search.css";
 
 export default function Search() {
     let [keyword, setKeyword] = useState("");
 
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(showResult);
+
+    function showResult(response) {
+        console.log(response.data);
+    }
+
     function handleSubmit(event) {
         event.preventDefault();
-        alert(`The word you are searching is ${keyword}`);
+        // alert(`The word you are searching is ${keyword}`);
     }
 
     function handleKeywordChange(event) {
