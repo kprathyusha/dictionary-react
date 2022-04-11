98258d97b0238beec9ff82d6913b09d11cae2607
kprathyusha <pratyu.kulkarni@gmail.com>
2022-04-11 10:29:14 -0400

    updated the issue with api looping

--- src/Search.js
+++ src/Search.js
@@ -1,20 +1,24 @@
 import axios from "axios";
 import React, { useState } from "react";
 import "./Search.css";
+import Results from "./Results";
 
 export default function Search() {
     let [keyword, setKeyword] = useState("");
-
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(showResult);
+    let [results, setResults] = useState(null);
 
     function showResult(response) {
         console.log(response.data);
+        setResults(response.data[0]);
+        // console.log(response.data[0].meanings[0].definitions[0].definition);
     }
 
     function handleSubmit(event) {
         event.preventDefault();
         // alert(`The word you are searching is ${keyword}`);
+
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+        axios.get(apiUrl).then(showResult);
     }
 
     function handleKeywordChange(event) {
@@ -22,15 +26,18 @@ export default function Search() {
     }
 
     return (
-        <div className="Search">
-            <h1>Dictionary</h1>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="search"
-                    placeholder="Enter a word"
-                    onChange={handleKeywordChange}
-                />
-            </form>
+        <div>
+            <div className="Search">
+                <h1>Dictionary</h1>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="search"
+                        placeholder="Enter a word"
+                        onChange={handleKeywordChange}
+                    />
+                </form>
+                <Results results={results} />
+            </div>
         </div>
     );
 }
