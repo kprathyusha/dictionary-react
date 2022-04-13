2593098d9aee1c7a7364b42775de2baa3b9755d6
kprathyusha <pratyu.kulkarni@gmail.com>
2022-04-13 00:24:54 -0400

    added alt to img

--- src/Photos.js
+++ src/Photos.js
@@ -7,7 +7,13 @@ export default function Photos(props) {
         return (
             <section className="Photos">
                 {props.photos.map(function (photo, index) {
-                    return <img src={photo.src.small} key={index} />;
+                    return (
+                        <img
+                            src={photo.src.small}
+                            alt="img not available"
+                            key={index}
+                        />
+                    );
                 })}
             </section>
         );
