61f777634248848a6531ef926b8da616e654a290
kprathyusha <pratyu.kulkarni@gmail.com>
2022-04-12 22:44:28 -0400

    added api for displaying photos

--- src/Search.js
+++ src/Search.js
@@ -13,12 +13,24 @@ export default function Search() {
         // console.log(response.data[0].meanings[0].definitions[0].definition);
     }
 
+    function handlePexelsResponse(response) {
+        console.log(response.data);
+    }
+
     function handleSubmit(event) {
         event.preventDefault();
         // alert(`The word you are searching is ${keyword}`);
 
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
         axios.get(apiUrl).then(showResult);
+
+        const pexelsApikey =
+            "563492ad6f917000010000017bc55a9495c24e6bb52145e7f9564f1b";
+        let pexelsApiurl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
+        let headers = { Authorization: `Bearer ${pexelsApikey}` };
+        axios
+            .get(pexelsApiurl, { headers: headers })
+            .then(handlePexelsResponse);
     }
 
     function handleKeywordChange(event) {
@@ -28,7 +40,6 @@ export default function Search() {
     return (
         <div>
             <div className="Search">
-               
                 <form onSubmit={handleSubmit}>
                     <input
                         type="search"
